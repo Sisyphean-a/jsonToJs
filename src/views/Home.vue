01dd81f9027b4958fca91acbdab58a3ab6f74317
xixifu <xia362108178@gmail.com>
2025-04-24 14:47:12 +0800

    init

---
+++ src/views/Home.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <json-view :json="json" />
+  </div>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+import jsonView from '@/components/jsonView.vue'
+
+const json = ref({
+  name: '张三',
+  age: 18,
+  gender: '男',
+  address: {
+    province: '北京',
+    city: '北京',
+    district: '朝阳区',
+  },
+})
+</script>
+
+<style lang="scss" scoped></style>
