fda1f508d8dec607f42e3e95685745544ccac879
xixifu <xia362108178@gmail.com>
2025-04-28 10:38:36 +0800

    提取svg

--- src/components/JsonView.vue
+++ src/components/JsonView.vue
@@ -46,24 +46,12 @@
               @click.stop="copyJson"
               title="复制JSON"
             >
-              <svg
-                viewBox="0 0 24 24"
+              <img
+                src="@/assets/icons/copy.svg"
+                alt="复制"
                 width="14"
                 height="14"
-                stroke="currentColor"
-                stroke-width="2"
-                fill="none"
-              >
-                <rect
-                  x="9"
-                  y="9"
-                  width="13"
-                  height="13"
-                  rx="2"
-                  ry="2"
-                ></rect>
-                <path d="M5 15H4a2 2 0 0 1-2-2V4a2 2 0 0 1 2-2h9a2 2 0 0 1 2 2v1"></path>
-              </svg>
+              />
             </button>
           </p>
           <div
